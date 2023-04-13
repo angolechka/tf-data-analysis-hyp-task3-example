77c32f85c9093819b91e990f39950bfdd991e613
Anna Golubeva <110536476+angolechka@users.noreply.github.com>
2023-04-13 22:55:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,10 +4,11 @@ from statsmodels.stats.weightstats import ztest as ztest
 
 chat_id = 266642884  # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array, y: int) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 0.06
+    y = 300
     stat, pval = ztest(x, value=y, alternative='smaller')
     return pval < alpha # Ваш ответ, True или False
